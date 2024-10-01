c10d91d44e3b5951ae619f841262cd6a6cb9d9b4
Anuj Rawat <37796611+ajtazer@users.noreply.github.com>
2024-10-01 17:06:31 +0530

    ignore sheet update ven scraping fail
    dont update ven data is [0,0,0]

--- sheets.py
+++ sheets.py
@@ -8,15 +8,12 @@ def setup_google_sheets():
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive.file"
     ]
-    
-    # Load credentials from the environment variable (GOOGLE_SHEETS_CREDENTIALS)
+
     creds_dict = json.loads(os.getenv('GOOGLE_SHEETS_CREDENTIALS'))
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-    
-    # Authorize the client with the credentials
+
     client = gspread.authorize(creds)
-    
-    # Open the spreadsheet by its ID
+
     sheet_id = '1s9sGp8t9SieFUGzdjxeC_c30BbnDO-912DUhMvUZfUk'
     return client.open_by_key(sheet_id)
 
@@ -32,20 +29,24 @@ def fetch_usernames(start_row, end_row):
 def update_sheet(username, easy_data, medium_data, hard_data):
     try:
         sheet = setup_google_sheets().get_worksheet(0)
-    
+
         existing_users = sheet.col_values(1)
 
         if username in existing_users:
-            row_index = existing_users.index(username) + 1  # +1 for 1-indexing
-            sheet.update_cell(row_index, 2, easy_data)  # Update Easy column
-            sheet.update_cell(row_index, 3, medium_data)  # Update Medium column
-            sheet.update_cell(row_index, 4, hard_data)  # Update Hard column
+            row_index = existing_users.index(username) + 1
+            current_data = sheet.row_values(row_index)[1:4]
+            current_data = [int(val) if val.isdigit() else 0 for val in current_data]
+            if current_data == [0, 0, 0]:
+                print(f"Existing data is 0, 0, 0 for {username}, skipping update.")
+                return
+            sheet.update_cell(row_index, 2, easy_data)  
+            sheet.update_cell(row_index, 3, medium_data)  
+            sheet.update_cell(row_index, 4, hard_data)  
         else:
-            # Append new row with username and challenges
+
             sheet.append_row([username, easy_data, medium_data, hard_data])
 
         print("Data has been written to Google Sheets.")
 
     except Exception as e:
         print(f"An error occurred while writing to Google Sheets: {e}")
-
